11f75f19bfba62e6715135d166e0f49e8ff8f65b
unsivil <unsivilaudio@gmail.com>
2020-11-06 10:18:36 -0700

    wrap up app before testing

--- services/cache.js
+++ services/cache.js
@@ -4,10 +4,21 @@ const util = require('util');
 
 const redisURL = 'redis://localhost:6379';
 const client = redis.createClient(redisURL);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
+mongoose.Query.prototype.cache = function (options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+
+    return this;
+};
+
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+
     const key = JSON.stringify(
         Object.assign({}, this.getFilter(), {
             collection: this.mongooseCollection.name,
@@ -15,7 +26,7 @@ mongoose.Query.prototype.exec = async function () {
     );
 
     // See if we have a value for 'key' in redis
-    const cacheValue = await client.get(key);
+    const cacheValue = await client.hget(this.hashKey, key);
 
     // If we do, return that
     if (cacheValue) {
@@ -28,10 +39,15 @@ mongoose.Query.prototype.exec = async function () {
 
     // Otherwise issue the query and store the result in redis
     const result = await exec.apply(this, arguments);
-    client.set(key, JSON.stringify(result));
 
-    console.log('\n************* FROM MONGO **************\n');
-    console.log(result);
+    client.hset(this.hashKey, key, JSON.stringify(result));
+    client.expire(this.hashKey, 10);
 
     return result;
+};
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    },
 };

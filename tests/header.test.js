772383405194bc94de95acb0df49866ff0fe03cb
unsivil <unsivilaudio@gmail.com>
2020-11-06 12:08:45 -0700

    first tests

---
+++ tests/header.test.js
@@ -0,0 +1,28 @@
+import puppeteer from 'puppeteer';
+
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false,
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+});
+
+test('The header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+});
+
+test('Clicking login starts oauth flow', async () => {
+    await page.click('.right a');
+    const url = await page.url();
+    // expect(url.startsWith('https://accounts.google.com/o/oauth')).toBeTruthy();
+    expect(url).toMatch(/accounts\.google\.com/);
+});
